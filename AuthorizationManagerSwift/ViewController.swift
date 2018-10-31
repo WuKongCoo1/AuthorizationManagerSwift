be1058b192dfb890307c8f591a6cff4ffc2eaa03
Jacky <wu_kong_coo1@163.com>
2018-10-31 15:56:21 +0800

    Initial Commit

---
+++ AuthorizationManagerSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthorizationManagerSwift
+//
+//  Created by Jacky on 2018/10/31.
+//  Copyright © 2018 FinupGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
